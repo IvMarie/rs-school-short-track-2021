f2145837c6fda05f36eff5ddd3ddb98f8b02382a
IvMarie <marie555iv@gmail.com>
2021-04-11 14:30:58 +0400

    feat: add function of remove from list

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -17,8 +17,22 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  let first = l;
+  while (first.value === k) {
+    first = first.next;
+  }
+  let current = first;
+  if (current) {
+    while (current.next) {
+      if (current.next.value !== k) {
+        current = current.next;
+      } else {
+        current.next = current.next.next;
+      }
+    }
+  }
+  return first;
 }
 
 module.exports = removeKFromList;
