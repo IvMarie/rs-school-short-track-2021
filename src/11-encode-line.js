fd57e3e2175d9328b692b998825034126be27326
IvMarie <marie555iv@gmail.com>
2021-04-11 22:09:17 +0400

    feat: add function of encode line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str.length === 0) {
+    return str;
+  }
+  const result = [];
+  let count = 1;
+  str.split('').forEach((el, i, arr) => {
+    if (el === arr[i + 1]) {
+      count++;
+    } else if (count === 1) {
+      result.push(el);
+    } else {
+      result.push(`${count}${el}`);
+      count = 1;
+    }
+  });
+  return result.join('');
 }
 
 module.exports = encodeLine;
